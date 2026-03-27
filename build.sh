7c683f5d5c1a4fbeb51d78002285d513ded08155
nbits.y <nbits.y@163.com>
2026-03-27 20:36:59 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+TarsFramework=https://github.com/nbitsy/TarsFramework.git
+TarsCpp=https://github.com/nbitsy/TarsCpp.git
+TarsProtocol=https://github.com/nbitsy/TarsProtocol.git
+TarsWeb=https://github.com/nbitsy/TarsWeb.git
+TarsGo=https://github.com/nbitsy/TarsGo.git
+
+TarsFramework_BRANCH=master
+TarsCpp_BRANCH=master
+TarsProtocol_BRANCH=master
+TarsWeb_BRANCH=master
+TarsGo_BRANCH=master
+
+ROOT="$(cd $(dirname $0); pwd)"
+INSTALL_PATH=$ROOT/app
+
+# 拉取TarsFramework
+git clone -b $TarsFramework_BRANCH $TarsFramework
+
+rm -rf $INSTALL_PATH TarsFramework/tarscpp TarsFramework/tarscpp/servant/protocol
+
+cd TarsFramework && git clone -b $TarsCpp_BRANCH $TarsCpp tarscpp && cd -
+cd TarsFramework/tarscpp/servant && git clone -b $TarsProtocol_BRANCH $TarsProtocol protocol && cd -
+
+# 编译TarsFramework
+cd TarsFramework && ./build.sh cleanall && TARS_INSTALL_PATH=$INSTALL_PATH ./build.sh all && cd -
+
+cd TarsFramework/build && make install && cd -
+
+# 拉取TarsWeb
+# git clone -b $TarsWeb_BRANCH $TarsWeb $INSTALL_PATH/deploy/web 
+
+# 拉取TarsGo
+# git clone -b $TarsGo_BRANCH $TarsGo TarsGo
+

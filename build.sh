fe59fc236211bea73b13f9f4c95d0d118c67944a
nbits.y <nbits.y@163.com>
2026-03-27 21:10:43 +0800

    支持不拉取代码只是更新

--- build.sh
+++ build.sh
@@ -15,13 +15,33 @@ TarsGo_BRANCH=master
 ROOT="$(cd "$(dirname "$0")"; pwd)"
 INSTALL_PATH="${1:-$ROOT/app}"
 
-# 拉取TarsFramework
-git clone -b $TarsFramework_BRANCH $TarsFramework
+clone_or_update() {
+    local repo_url="$1"
+    local branch="$2"
+    local target_dir="$3"
 
-rm -rf "$INSTALL_PATH" TarsFramework/tarscpp TarsFramework/tarscpp/servant/protocol
+    if [ -d "$target_dir/.git" ]; then
+        echo "[update] $target_dir exists, update from $branch"
+        (
+            cd "$target_dir" || exit 1
+            git fetch origin "$branch" && git pull --ff-only origin "$branch"
+        )
+        return 0
+    fi
 
-cd TarsFramework && git clone -b $TarsCpp_BRANCH $TarsCpp tarscpp && cd -
-cd TarsFramework/tarscpp/servant && git clone -b $TarsProtocol_BRANCH $TarsProtocol protocol && cd -
+    if [ -d "$target_dir" ]; then
+        echo "[skip] $target_dir exists but is not a git repo, skip clone"
+        return 0
+    fi
+
+    git clone -b "$branch" "$repo_url" "$target_dir"
+}
+
+# 拉取或更新 TarsFramework
+clone_or_update "$TarsFramework" "$TarsFramework_BRANCH" "TarsFramework"
+
+clone_or_update "$TarsCpp" "$TarsCpp_BRANCH" "TarsFramework/tarscpp"
+clone_or_update "$TarsProtocol" "$TarsProtocol_BRANCH" "TarsFramework/tarscpp/servant/protocol"
 
 # 编译TarsFramework
 cd TarsFramework && ./build.sh cleanall && TARS_INSTALL_PATH="$INSTALL_PATH" ./build.sh all && cd -
@@ -29,7 +49,8 @@ cd TarsFramework && ./build.sh cleanall && TARS_INSTALL_PATH="$INSTALL_PATH" ./b
 cd TarsFramework/build && make install && cd -
 
 # 拉取TarsWeb
-git clone -b $TarsWeb_BRANCH $TarsWeb "$INSTALL_PATH/deploy/web" 
+mkdir -p "$INSTALL_PATH/deploy"
+clone_or_update "$TarsWeb" "$TarsWeb_BRANCH" "$INSTALL_PATH/deploy/web"
 # 完后可以到$INSTALL_PATH/deploy/目录执行安装程序或者脚本 
 
 # 拉取TarsGo
